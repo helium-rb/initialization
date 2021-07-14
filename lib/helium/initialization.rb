e1111468d7ca4ab43ba1a74df85dd422e755d804
Stanislaw Klajn <sklajn@gmail.com>
2021-07-14 01:17:57 +0100

    Basic initialization hooks

--- lib/helium/initialization.rb
+++ lib/helium/initialization.rb
@@ -2,7 +2,34 @@ require "helium/initialization/version"
 
 module Helium
   module Initialization
-    class Error < StandardError; end
-    # Your code goes here...
+    def self.included(mod)
+      mod.extend ClassMethods
+      mod.prepend Initialize
+    end
+  end
+
+  module ClassMethods
+    def before_initialize(&block)
+      initialization[:before] << block
+    end
+
+    def after_initialize(&block)
+      initialization[:after] << block
+    end
+
+    def initialization
+      @initialization ||= {
+        before: [],
+        after: []
+      }.freeze
+    end
+  end
+
+  module Initialize
+    def initialize(*)
+      self.class.initialization[:before].each { |block| instance_exec(&block) }
+      super
+      self.class.initialization[:after].each { |block| instance_exec(&block) }
+    end
   end
 end
--- spec/helium/initialization_spec.rb
+++ spec/helium/initialization_spec.rb
@@ -1,9 +1,52 @@
 RSpec.describe Helium::Initialization do
-  it "has a version number" do
-    expect(Helium::Initialization::VERSION).not_to be nil
+  let(:klass) do
+    foo = Module.new do
+      attr_reader :foo
+
+      def self.included(mod)
+        mod.before_initialize { @foo = :foo }
+      end
+    end
+
+    bar = Module.new do
+      attr_reader :bar
+
+      def self.included(mod)
+        mod.after_initialize { @bar = :bar }
+      end
+    end
+
+    Class.new do
+      include Helium::Initialization
+
+      include foo
+      include bar
+
+      attr_reader :foo_in_init, :bar_in_init, :args
+
+      def initialize(*args)
+        @args = args
+        @foo_in_init = foo
+        @bar_in_init = bar
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:args) { Array.new(rand 1..10) { rand(1..10) } }
+
+  subject { klass.new(*args) }
+
+  it "executes before_initialize before initialization" do
+    expect(subject.foo).to eq :foo
+    expect(subject.foo_in_init).to eq :foo
+  end
+
+  it "executes after_initialize after the initialization" do
+    expect(subject.bar).to eq :bar
+    expect(subject.bar_in_init).to eq nil
+  end
+
+  it "doesn't change signature of the initialization" do
+    expect(subject.args).to eq args
   end
 end
